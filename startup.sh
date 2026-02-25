96b8c1b2b988f8d5bbcddb12945b04429ed5b8e5
Calcuiii <salsa.jjk05@gmail.com>
2026-02-25 10:55:52 +0700

    fix ssl mysql and startup

--- startup.sh
+++ startup.sh
@@ -4,27 +4,28 @@ cd /home/site/wwwroot
 
 composer install --no-dev --optimize-autoloader
 
-php artisan key:generate --force
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Konfigurasi nginx untuk Laravel
-echo "server {
+# Konfigurasi nginx
+cat > /etc/nginx/sites-enabled/default << 'EOF'
+server {
     listen 8080;
     root /home/site/wwwroot/public;
     index index.php index.html;
 
     location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
+        try_files $uri $uri/ /index.php?$query_string;
     }
 
     location ~ \.php$ {
         fastcgi_pass 127.0.0.1:9000;
-        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
         include fastcgi_params;
     }
-}" > /etc/nginx/sites-enabled/default
+}
+EOF
 
 service nginx restart
